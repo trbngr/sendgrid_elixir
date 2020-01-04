9a890f732faeb31a34008fdab43564ad11ade9e0
Chris Martin <chris.martin@oforce.com>
2020-01-03 17:54:22 -0700

    implement jason encoder for asm

--- lib/sendgrid/email/asm.ex
+++ lib/sendgrid/email/asm.ex
@@ -1,3 +1,17 @@
 defmodule Sendgrid.Email.Asm do
   defstruct group_id: nil, groups_to_display: nil
+
+  defimpl Jason.Encoder do
+    def encode(%{group_id: group_id, groups_to_display: groups_to_display}, opts) do
+      params =
+        %{}
+        |> put_value(:group_id, group_id)
+        |> put_value(:groups_to_display, groups_to_display)
+
+      Jason.Encode.map(params, opts)
+    end
+
+    defp put_value(map, _key, nil), do: map
+    defp put_value(map, key, value), do: Map.put(map, key, value)
+  end
 end
